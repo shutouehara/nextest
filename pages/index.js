2ccd85764622263abcfb9e9c24dd206b2d0442bc
shutouehara <shuto.u2f@gmail.com>
2021-11-20 23:59:21 +0900

    repush

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import styles from '../styles/Home.module.css'
+import Styles from '../styles/index.module.css'
 
 export default function Home() {
   return (
     <div>
-      <h1>tesuto</h1>
+      <h1 className={Styles.mytitle}>tesuto</h1>
+      <p>このページはテストです。これからnext.js頑張ります！</p>
     </div>
   )
 }
