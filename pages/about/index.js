7473e7132cfd1d7a96462c30a8cc045a648d5aa0
shutouehara <shuto.u2f@gmail.com>
2021-11-21 08:48:55 +0900

    ser btns

---
+++ pages/about/index.js
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+export default function About() {
+    return (
+        <>
+            <h1>About Page</h1>
+            <Link href={'/'}><button>TOP</button></Link>
+            <Link href={'/about'}><button>ABOUT</button></Link>
+        </>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
 import styles from '../styles/Home.module.css'
 import Styles from '../styles/index.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div>
+    <>
       <h1 className={Styles.mytitle}>tesuto</h1>
       <p>このページはテストです。これからnext.js頑張ります！</p>
-    </div>
+            <Link href={'/'}><button>TOP</button></Link>
+            <Link href={'/about'}><button>ABOUT</button></Link>
+    </>
   )
 }
