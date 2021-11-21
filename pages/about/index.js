bf52fd1ba2c278c3c7148e214ff2cdd0556ee45e
shutouehara <shuto.u2f@gmail.com>
2021-11-21 11:26:44 +0900

    add swr

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,13 +1,21 @@
 import Link from 'next/link'
+import useSWR from 'swr'
 
 import Header from '../components/header'
 import Content from '../components/content'
 
+const fetcher = url => fetch(url).then(r => r.json())
+
 export default function About() {
+    const {data,error} = useSWR('/api/hello', fetcher)
+    if (error) return <div>failed to load</div>
+    if (!data) return <div>loading...</div>
+  
     return (
         <>
             <Header title={'ABOUT'}></Header>
             <h1>About Page</h1>
+            <p>{data.name}</p>
             <Link href={'/'}><button>TOP</button></Link>
             <Link href={'/about'}><button>ABOUT</button></Link>
             <Content></Content>
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,26 @@
 import styles from '../styles/Home.module.css'
 import Styles from '../styles/index.module.css'
 import Link from 'next/link'
+import useSWR from 'swr'
 
 import Header from './components/header'
 import Content from './components/content'
 
+let title = 'TestSite'
+
+const fetcher = url => fetch(url).then(r => r.json())
+
 export default function Home() {
+  const {data,error} = useSWR('/api/hello', fetcher)
+  if (error) return <div>failed to load</div>
+  if (!data) return <div>loading...</div>
+
   return (
     <>
       <Header title={'TOP'}></Header>
       <h1 className={Styles.mytitle}>tesuto</h1>
       <p>このページはテストです。これからnext.js頑張ります！</p>
+      <p>{data.name}</p>
       <Link href={'/'}><button>TOP</button></Link>
       <Link href={'/about'}><button>ABOUT</button></Link>
       <Content></Content>
